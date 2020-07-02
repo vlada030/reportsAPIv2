3cf98779f0615eab2227de6b5831a32c478eb11c
Vlada DELL <vlada030@gmail.com>
2020-07-02 21:30:25 +0200

    nastavak - PUG fajlovi

--- controllers/reports.js
+++ controllers/reports.js
@@ -24,3 +24,12 @@ exports.getShiftReportsHTML = (req, res, next) => {
 
     res.status(200).render('shiftReports', {title: 'Smenski izveštaj o radu', path: 'shift', lang: 'ser'});
 };
+
+// @desc   Shift Reports
+// @route  GET /api/v2/reports/shift
+// @access Private
+
+exports.proba = (req, res, next) => {
+
+    res.status(200).render('create_product', {title: 'Smenski izveštaj o radu', path: 'none', lang: 'ser'});
+};
