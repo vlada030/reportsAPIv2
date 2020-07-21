760eed990d0859454260d8b3eea1c194e7274f41
Vlada DELL <vlada030@gmail.com>
2020-07-21 19:55:22 +0200

    sitna ispravka server.js = / ==='production'

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(formidableMiddleware());
 app.use(cookieParser());
 
 // pozivanje morgan loggera
-if (process.env.NODE_ENV = 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
