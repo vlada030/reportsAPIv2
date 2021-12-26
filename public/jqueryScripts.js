b1af01356b6ddf9b71dec951a8c37375c5ab0b15
Vlada DELL <vlada030@gmail.com>
2021-12-26 22:04:59 +0100

    zapocet unos polja za custom datum - input type='date' vs type='text'

--- public/jqueryScripts.js
+++ public/jqueryScripts.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
     // datum
     var formatedDate = new Date().getDate() + '.' + (new Date().getMonth() + 1) + '.' + new Date().getFullYear() + '.';
     $('#date').text(formatedDate);
+    // datum domaci
+    // document.querySelector('#datum').valueAsDate = new Date();
+    // console.log(document.querySelector('#datum').placeholder);
+    document.querySelector('#datum').value = new Intl.DateTimeFormat('sr-RS').format(new Date())
     // godina
     $(".year").text(new Date().getFullYear());
     // hamburger button
